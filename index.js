4ff2584d4c2d1984f7fcbe87f69a6a5bbc278f27
Adnan Reza <adnan.reza@outlook.com>
2021-05-31 15:40:37 -0700

    Created enpoint definitions

--- index.js
+++ index.js
@@ -10,3 +10,30 @@ const genres = [
     { id: 2, name: 'Comedy' },
     { id: 3, name: 'Drama' },
 ];
+
+/**
+ * @route GET api/genres
+ * @desc Returns all genres
+ * @access Public
+ */
+
+/**
+ * @route GET api/genres
+ * @desc Returns all genres
+ * @access Public
+ */
+
+/**
+ * @route GET api/genres
+ * @desc Returns all genres
+ * @access Public
+ */
+
+/**
+ * @route DELETE api/genre/
+ * @desc Returns all genres
+ * @access Public
+ */
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {console.log(`Listening on port ${port}...`)})
