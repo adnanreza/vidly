2c15375f05c81414aad841672f3c5c380d2deddc
Adnan Reza <adnan.reza@outlook.com>
2021-05-31 15:48:26 -0700

    Implement GET routes for genres

--- index.js
+++ index.js
@@ -16,24 +16,49 @@ const genres = [
  * @desc Returns all genres
  * @access Public
  */
+app.get('/api/genres', (req, res) => {
+    res.send(genres);
+})
 
 /**
- * @route GET api/genres
- * @desc Returns all genres
+ * @route GET api/genres/:id
+ * @desc Returns individual genre by id
+ * @access Public
+ */
+app.get('/api/genres/:id', (req, res) => {
+    const genre = genres.find(g => parseInt(req.params.id) === g.id);
+    if(!genre) return res.status(404).send('Genre with specified ID not found.');
+    res.send(genre);   
+}) 
+
+/**
+ * @route POST api/genres
+ * @desc Create new genre
  * @access Public
  */
 
 /**
- * @route GET api/genres
- * @desc Returns all genres
+ * @route PUT api/genres/:id
+ * @desc Update individual genre by id
  * @access Public
  */
 
 /**
- * @route DELETE api/genre/
- * @desc Returns all genres
+ * @route DELETE api/genres/:id
+ * @desc DELETE genre by id
  * @access Public
  */
+
+/**
+ * @desc Validates genre using Joi
+ * @return valid or invalid field
+ */
+function validateGenre(genre) {
+    const schema = {
+        name: Joi.string().min(3).required()
+    }
+    return Joi.validate(genre, schema);
+}
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {console.log(`Listening on port ${port}...`)})
