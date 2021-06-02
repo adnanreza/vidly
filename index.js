6539c833313893dac0991dbbaac4b38c33ab503b
Adnan Reza <adnan.reza@outlook.com>
2021-06-02 16:05:45 -0700

    Implemented rentals API

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 const genres = require('./routes/genres')
 const customers = require('./routes/customers')
 const movies = require('./routes/movies')
+const rentals = require('./routes/rentals')
 
 // Load env vars
 dotenv.config({ path: './config.env' });
@@ -19,6 +20,7 @@ app.use(express.json());
 app.use('/api/genres', genres);
 app.use('/api/customers', customers);
 app.use('/api/movies', movies);
+app.use('/api/rentals', rentals);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {console.log(`Listening on port ${port}...`)})
---
+++ routes/rentals.js
@@ -0,0 +1,58 @@
+const { Rental, validateRental } = require('../models/Rental')
+const { Movie } = require('../models/Movie');
+const { Customer } = require('../models/Customer');
+const mongoose = require('mongoose');
+const express = require('express');
+const router = express.Router();
+
+
+/**
+ * @route GET api/rentals
+ * @desc Returns all rentals
+ * @access Public
+ */
+router.get('/', async (req, res) => {
+    const rentals = await Rental.find().sort('-dateOut');
+    res.send(rentals);
+})
+
+/**
+ * @route POST api/rentals
+ * @desc Create new rental
+ * @access Public
+ */
+ router.post('/', async (req, res) => {
+    const validationResult = validateRental(req.body);
+    if(validationResult.error){
+        return res.status(400).send(validationResult.error.details[0].message);
+    }
+
+    const customer = await Customer.findById(req.body.customerId);
+    if(!customer) return res.status(400).send('No such Customer found');
+
+    const movie = await Movie.findById(req.body.movieId);
+    if(!movie) return res.status(400).send('No such movie found');
+
+    if(movie.numberInStock === 0) return res.status(400).send('Movie not in stock');
+
+    let rental = new Rental({ 
+        customer: {
+            _id: customer._id,
+            name: customer.name,
+            phone: customer.phone
+        },
+        movie: {
+            _id: movie._id,
+            title: movie.title,
+            dailyRentalRate: movie.dailyRentalRate,
+        }
+    });
+    rental = await rental.save();
+
+    movie.numberInStock--;
+    movie.save();
+    
+    res.send(rental);
+})
+
+module.exports = router;
