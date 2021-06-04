6af3e2de116fcaaadc1a309204791285fdb8564f
Adnan Reza <adnan.reza@outlook.com>
2021-06-03 21:14:38 -0700

    Added auth route and implemented login endpoint

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const customers = require('./routes/customers')
 const movies = require('./routes/movies')
 const rentals = require('./routes/rentals')
 const users = require('./routes/users')
+const auth = require('./routes/auth')
 
 // Load env vars
 dotenv.config({ path: './config.env' });
@@ -25,6 +26,7 @@ app.use('/api/customers', customers);
 app.use('/api/movies', movies);
 app.use('/api/rentals', rentals);
 app.use('/api/users', users);
+app.use('/api/auth', auth);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {console.log(`Listening on port ${port}...`)})
---
+++ routes/auth.js
@@ -0,0 +1,44 @@
+const _ = require('lodash');
+const Joi = require('joi');
+const bcrypt = require('bcrypt');
+const express = require('express');
+const mongoose = require('mongoose');
+const { User } = require('../models/User');
+const router = express.Router();
+
+/**
+ * @route POST api/auth
+ * @desc Create new user
+ * @access Public
+ */
+router.post('/', async (req, res) => {
+    const validationResult = validate(req.body);
+    if(validationResult.error){
+        return res.status(400).send(validationResult.error.details[0].message);
+    }
+
+    let user = await User.findOne({ email: req.body.email });
+    if(!user) return res.status(400).send('Invalid email or password');
+    
+    const validPassword = await bcrypt.compare(req.body.password, user.password);
+    if(!validPassword) return res.status(400).send('Invalid email or password');
+    
+    res.send(true);
+})
+
+/**
+ * @desc Validates req
+ * @return valid or invalid
+ */
+ function validate(req) {
+    const schema = Joi.object({
+      email: Joi.string().min(5).max(255).required().email(),
+      password: Joi.string().min(5).max(255).required(),
+    });
+  
+    return schema.validate(req);
+  }
+
+
+
+module.exports = router;
